bd543410071a16c1e91d06c318d9a4cd1a89fdea
Linjie Zhang <143931753+ISQIShI@users.noreply.github.com>
2025-03-14 11:08:21 +0800

    事件中心-Csharp

---
+++ GameEventType.cs
@@ -0,0 +1,18 @@
+﻿public enum GameEventType
+{
+    //事件类型合集
+    BeginGame,
+    ExitGame,
+    //场景相关
+
+    closeDoor,//关门
+    loadRequestEvent,//加载新场景
+    //人物相关
+    playerHealthReduce,//人物血量减少
+
+    beginBattle,//开始战斗
+    //UI
+    fadeCanves
+
+
+}
